3c99dc0b5917b49c7d0d836553fc70e19ceb49c4
昝崇崇 <213201535@seu.edu.cn>
2022-08-19 14:32:14 +0800

    modified:   HelloWorld/HelloWorld/__pycache__/urls.cpython-39.pyc 	modified:   HelloWorld/HelloWorld/__pycache__/views.cpython-39.pyc 	modified:   HelloWorld/HelloWorld/urls.py 	modified:   HelloWorld/HelloWorld/views.py 	modified:   HelloWorld/MarketData/__pycache__/__init__.cpython-39.pyc 	modified:   HelloWorld/MarketData/__pycache__/urls.cpython-39.pyc 	modified:   HelloWorld/MarketData/__pycache__/views.cpython-39.pyc

--- HelloWorld/HelloWorld/urls.py
+++ HelloWorld/HelloWorld/urls.py
@@ -6,11 +6,7 @@ urlpatterns = [
     path('hello/', views.hello),
     path('login/', views.login),
     path('register/', views.register),
-
     path("stock/", include("Stock.urls")),
-<<<<<<< HEAD
     path("strategy/", include("Strategy.urls")),
-=======
     path("UDdistribution/", include("MarketData.urls")),
->>>>>>> c51ca6964c26720b9e21a89faad614daf23b30f2
 ]
