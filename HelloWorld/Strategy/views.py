75a7971117b13b4767fb8494092ebf88ab4f51d4
昝崇崇 <213201535@seu.edu.cn>
2022-08-26 09:10:53 +0800

    improve collection list

--- HelloWorld/Strategy/views.py
+++ HelloWorld/Strategy/views.py
@@ -23,6 +23,7 @@ def list(request):
     for item in results:
         tmp={'num':item[0],'title':item[1],'text':item[2],'tag1':item[3],'tag2':item[4],'tag3':item[5]}
         res.append(tmp)
+    db.close()
     return HttpResponse(json.dumps(res))
 
 @csrf_exempt
@@ -42,8 +43,12 @@ def sma(request):
     hold_result = hold_result.to_json(orient = 'records')
     trade_result = trade_result.to_json(orient = 'records')
     value_ratio = value_ratio.to_json(orient = 'records')
-
-    return HttpResponse(json.dumps({'hold_result':hold_result,'trade_result':trade_result,'value_ratio':value_ratio,'indicator_list':indicator_list})) 
+    res=[]
+    res.append(hold_result)
+    res.append(trade_result)
+    res.append(value_ratio)
+    res.append(indicator_list)
+    return HttpResponse(res)
 
 @csrf_exempt
 def turtle(request):
